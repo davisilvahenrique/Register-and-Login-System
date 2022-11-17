7f864b7a68be5a2e1c51feb773ee1e8ec57be6ef
Davi Henrique <104919646+davisilvahenrique@users.noreply.github.com>
2022-11-17 16:30:13 -0300

    Update logado.js

--- logado/logado.js
+++ logado/logado.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem('token') == null) {
     alert('Você precisa estar logado para acessar essa pagina')
-    window.location.href = 'http://localhost:63342/PhpstormProjects/login/login/login.html?_ijt=q00o59homm1u6vo06ga895rucb&_ij_reload=RELOAD_ON_SAVE'
+    window.location.href = 'http://localhost:63342/PhpstormProjects/Register-and-Login-System/login/login.html?_ijt=q00o59homm1u6vo06ga895rucb&_ij_reload=RELOAD_ON_SAVE'
 }
 
 let userLogado = JSON.parse(localStorage.getItem('userLogado'));
@@ -11,5 +11,5 @@ logado.innerHTML = 'Bem Vindo ' + userLogado.user + '!'
 function sair() {
     localStorage.removeItem('token')
     localStorage.removeItem('userLogado')
-    window.location.href = 'http://localhost:63342/PhpstormProjects/login/login/login.html?_ijt=q00o59homm1u6vo06ga895rucb&_ij_reload=RELOAD_ON_SAVE'
+    window.location.href = 'http://localhost:63342/PhpstormProjects/Register-and-Login-System/login/login.html?_ijt=q00o59homm1u6vo06ga895rucb&_ij_reload=RELOAD_ON_SAVE'
 }
